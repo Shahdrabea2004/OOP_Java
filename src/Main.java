068bc63cb807e92a90948ebe90f11c44e022fb68
Shahdrabea2004 <shahdrabiaa56@gmail.com>
2025-07-10 12:44:57 +0300

    update: description of change

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
-    //shahd
     public static void main(String[] args) {
-        System.out.println(Student.college);
+        Student student1=new Student("shahd");
+        Student student2=new Student("shahd");
+        Student student3=new Student("shahd");
+        System.out.println(student1.toString());
+        System.out.println(student2.toString());
+        System.out.println(student3.toString());
+        //System.out.println(Student.college);
 
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,22 @@
 public class Student {
-    private int id;
+    private String id;
+    private static int no=0;
     private String name;
 
     static String college="IT";
 
-    public Student(int id, String name) {
-        this.id = id;
+    public Student(String name) {
+        ++no;
+        this.id = "42210"+no;
         this.name = name;
+    }
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", college='" + college + '\'' +
+                '}';
     }
 }
