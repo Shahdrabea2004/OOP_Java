580b6a2847b44707dceae86361fbbe339bdce8fd
Shahdrabea2004 <shahdrabiaa56@gmail.com>
2025-07-10 11:35:45 +0300

    update main

---
+++ OOP Java/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    private int id;
+    private String name;
+
+  static String college="IT";
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+
+    }
+}
