9fec229f2e4033872cd3eb84dd0f90314627bd26
Shahdrabea2004 <shahdrabiaa56@gmail.com>
2025-07-10 14:06:25 +0300

    update All

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        Student student1=new Student("shahd");
-        Student student2=new Student("shahd");
-        Student student3=new Student("shahd");
+
+        //Static Variable
+        Student student1=new Student("shahd","12359");
+        Student student2=new Student("Abdo","123445");
+        System.out.println(student1.isLoggedIn("422101","12359"));//true
+        System.out.println(Student.validatePassword("123"));//false
+        System.out.println(Student.validatePassword("123896"));//true
+        System.out.println(student2.isLoggedIn("422102","1234"));//false
         System.out.println(student1.toString());
         System.out.println(student2.toString());
-        System.out.println(student3.toString());
+
         //System.out.println(Student.college);
+
+        //static method
+        /*
+        git add .
+        git commit -m "update All"
+        git push
+
+         */
 
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -2,13 +2,30 @@ public class Student {
     private String id;
     private static int no=0;
     private String name;
-
     static String college="IT";
-
-    public Student(String name) {
+    private String password;
+    private boolean loggedIn;
+    private static int minPassword=6;
+    public Student(String name,String password) {
+        if (!validatePassword(password)) {
+            System.out.println("Warning: Weak password!");
+        }
         ++no;
         this.id = "42210"+no;
         this.name = name;
+        this.password=password;
+    }
+
+    public boolean isLoggedIn(String id,String password){
+        if(id.equals(this.id) &&password.equals(this.password)){
+            loggedIn=true;
+            return true;
+        }
+        return false;
+    }
+    public static boolean validatePassword(String password){
+        if(password.length()>=minPassword)return true;
+        return false;
     }
 
     @Override
