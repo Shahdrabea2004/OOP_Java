9b1118d36b4fd490d24bbe705c2f8debb37aabd0
Shahdrabea2004 <shahdrabiaa56@gmail.com>
2025-07-17 15:59:07 +0300

    update examples2

--- src/Examples2/Main.java
+++ src/Examples2/Main.java
@@ -4,11 +4,20 @@ public class Main {
     static void fun(Test obj){
         obj.number++;
     }
+    static int fun(int a,int b){
+        int ans=a+b;
+        return ans;
+    }
     public static void main(String []args){
        Test test = new Test();
        test.number=199;
        fun(test);
         System.out.println(test.number);
+
+
+        int a=5,b=5;
+        System.out.println(fun(a,b));
+
     }
 
 }
